7644cef51abf78c1627f38f3fdc3bd91adfd33cd
kevin-y <kevin-y@users.noreply.github.com>
2015-05-19 17:54:35 +0800

    modified MailUitls.java

--- src/main/java/org/keviny/gallery/util/MailUtils.java
+++ src/main/java/org/keviny/gallery/util/MailUtils.java
@@ -9,6 +9,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -16,7 +18,7 @@ import java.util.Properties;
  */
 public class MailUtils {
 
-    public void sendWithFile() {
+	public void sendWithFile() {
         // Recipient's email ID needs to be mentioned.
         String to = "abcd@gmail.com";
 
@@ -43,8 +45,7 @@ public class MailUtils {
             message.setFrom(new InternetAddress(from));
 
             // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
             // Set Subject: header field
             message.setSubject("This is the Subject Line!");
@@ -70,7 +71,7 @@ public class MailUtils {
             multipart.addBodyPart(messageBodyPart);
 
             // Send the complete message parts
-            message.setContent(multipart );
+            message.setContent(multipart);
 
             // Send message
             Transport.send(message);
@@ -79,50 +80,111 @@ public class MailUtils {
             mex.printStackTrace();
         }
     }
+	
+	public static void send() {
+		  	String to = "test@example.com";
+	        String from = "test@example.com";
+	        String host = "smtp.exmail.qq.com";
+	        Properties props = new Properties();;
+	        // Setup mail server
+	        props.setProperty("mail.smtp.auth", "true");
+	        props.setProperty("mail.smtp.starttls.enable", "true");
+	        props.setProperty("mail.smtp.host", host);
+	        props.setProperty("mail.smtp.port", "465");
+	        props.put("mail.smtp.socketFactory.port", "465");
+	        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
+	        final String username = "test@example.com";
+	        final String password = "test";
+	        // Get the default Session object.
+	        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			  });
+
+	        try{
+	            // Create a default MimeMessage object.
+	            MimeMessage message = new MimeMessage(session);
+
+	            // Set From: header field of the header.
+	            message.setFrom(new InternetAddress(from));
+
+	            // Set To: header field of the header.
+	            message.addRecipient(Message.RecipientType.TO,
+	                    new InternetAddress(to));
+
+	            // Set Subject: header field
+	            message.setSubject("This is the Subject Line!");
+
+	            // Send the actual HTML message, as big as you like
+	            message.setContent("test",
+	                    "text/html" );
+
+	            // Send message
+	            Transport.send(message);
+	            System.out.println("Sent message successfully....");
+	        }catch (MessagingException mex) {
+	            mex.printStackTrace();
+	        }
+	}
+
+	public static void receive() {
+		
+		final String host = "pop.exmail.qq.com";
+		
+		Properties props = new Properties();
+		props.put("mail.pop3.host", host);
+		props.put("mail.pop3.port", "995");
+		props.put("mail.pop3.starttls.enable", "true");
+		props.put("mail.pop3.socketFactory.port", "995");
+        props.put("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        final String username = "test@example.com";
+        final String password = "test";
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+        try {
+			Store store = session.getStore("pop3s");
+			
+			store.connect(host, username, password);
+		    //create the folder object and open it
+		    Folder emailFolder = store.getFolder("INBOX");
+		    emailFolder.open(Folder.READ_ONLY);
+		    
+		    Message[] messages = emailFolder.getMessages();
+		    System.out.println("messages.length---" + messages.length);
+		    
+		    for (int i = 0, n = messages.length; i < n; i++) {
+		         Message message = messages[i];
+		         System.out.println("---------------------------------");
+		         System.out.println("Email Number " + (i + 1));
+		         System.out.println("Subject: " + message.getSubject());
+		         System.out.println("From: " + message.getFrom()[0]);
+		         System.out.println("Text: " + message.getContent().toString());
+
+		      }
+
+		      //close the store and folder objects
+		      emailFolder.close(false);
+		      store.close();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+	}
+	
     public static void main(String[] args) {
-        // Recipient's email ID needs to be mentioned.
-        String to = "abcd@gmail.com";
-
-        // Sender's email ID needs to be mentioned
-        String from = "web@gmail.com";
-
-        // Assuming you are sending email from localhost
-        String host = "localhost";
-
-        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-        try{
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Send the actual HTML message, as big as you like
-            message.setContent("<h1>This is actual message</h1>",
-                    "text/html" );
-
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        }catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
+    	send();
+    	//receive();
 
     }
 }
